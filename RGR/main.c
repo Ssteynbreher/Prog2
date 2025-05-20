82c5d551e38e56467ed80e02480b48bb3f57f886
Ssteynbreher <ssteynbreher@mail.ru>
2025-05-20 22:00:17 +0700

    Update main.c

--- RGR/main.c
+++ RGR/main.c
@@ -14,18 +14,18 @@ wchar_t* read_file(const char* filename) {
     }
 
     int capacity = 1024;
-    wchar_t* buffer = (wchar_t*)malloc(capacity);
+    wchar_t* buffer = (wchar_t*)malloc(capacity * sizeof(wchar_t));
     if (!buffer) {
         fclose(file);
         return NULL;
     }
 
     int pos = 0;
-    wchar_t c;
-    while ((c = fgetwc(file)) != EOF) {
+    wint_t c;
+    while ((c = fgetwc(file)) != WEOF) {
         if (pos >= capacity - 1) {
             capacity *= 2;
-            wchar_t* new_buffer = (wchar_t*)realloc(buffer, capacity);
+            wchar_t* new_buffer = (wchar_t*)realloc(buffer, capacity * sizeof(wchar_t));
             if (!new_buffer) {
                 free(buffer);
                 fclose(file);
@@ -33,14 +33,13 @@ wchar_t* read_file(const char* filename) {
             }
             buffer = new_buffer;
         }
-        buffer[pos++] = c;
+        buffer[pos++] = (wchar_t)c;
     }
-    buffer[pos] = '\0';
+    buffer[pos] = L'\0';
     fclose(file);
     return buffer;
 }
 
-// Запись текста в файл
 void write_file(const char* filename, const wchar_t* text) {
     FILE* file = fopen(filename, "w");
     if (!file) {
@@ -53,14 +52,12 @@ void write_file(const char* filename, const wchar_t* text) {
 
 int main() {
     setlocale(LC_ALL, "");
-    // Загрузка динамической библиотеки
     void* handle = dlopen("./libpolybius.so", RTLD_LAZY);
     if (!handle) {
         printf("Ошибка загрузки библиотеки: %s\n", dlerror());
         return 1;
     }
 
-    // Чтение исходного текста из файла input.txt
     wchar_t* input_text = read_file("input.txt");
     if (!input_text) {
         printf("Не удалось прочитать входной файл\n");
@@ -70,7 +67,6 @@ int main() {
 
     printf("Исходный текст: %ls\n", input_text);
 
-    // Шифрование
     wchar_t* encrypted = Polibi(input_text);
     if (!encrypted) {
         printf("Ошибка шифрования\n");
@@ -79,13 +75,9 @@ int main() {
         return 1;
     }
 
-    //
     printf("Зашифрованный текст: %ls\n", encrypted);
-
-    // Запись зашифрованного текста в файл output.txt
     write_file("output.txt", encrypted);
 
-    // Дешифрование
     wchar_t* decrypted = DePolibi(encrypted);
     if (!decrypted) {
         printf("Ошибка дешифрования\n");
@@ -97,23 +89,24 @@ int main() {
 
     printf("Дешифрованный текст: %ls\n", decrypted);
 
-    // Проверка совпадения
     if (wcscmp(input_text, decrypted) == 0) {
         printf("Исходный и дешифрованный тексты совпадают\n");
     } else {
         printf("Исходный и дешифрованный тексты НЕ совпадают\n");
     }
 
-    // Оценка криптостойкости
     printf("Оценка криптостойкости:\n");
-    printf("- Шифр Полибия уязвим к частотному анализу, так как каждая буква заменяется фиксированной парой.\n");
-    printf("- Рекомендации: использовать случайный порядок строк и столбцов в таблице для каждого сеанса или добавлять шумовые символы.\n");
+    printf("- Каждая буква заменяется фиксированной парой.\n");
+    printf("- Из-за статичной таблицы для всех сообщений исползуется одинаковые замены букв и пар символов, что делает шифр предсказуемым.\n");
+    printf("- Пробелы остаются неизменными в зашифрованном тексте, вследствие этого задачу злоумышленникам при дешифровке текста.\n");
+    printf("- Рекомендации: \n");
+    printf("- 1. Вместо фиксированной таблицы использовать каждый раз новую(перед применением сгенерировать случайную таблицу и передать ее как часть ключа). \n" );
+    printf(" Или же просто сгенерировать случайный порядок строк и столбцов.\n");  
+    printf("- 2. Шифровать пробелы тоже. Не оставлять пустоту, а также заменять их на пару символов, не используемую для других символов. \n");
 
-    // Освобождение памяти
     free(input_text);
     free(encrypted);
     free(decrypted);
-    dlclose(handle);
-
+    dlclose(handle); 
     return 0;
 }
